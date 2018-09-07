7690538f39fe104777d68482a533ada501bd27b1
Heliovic <1005031096@qq.com>
2018-09-07 10:25:53 +0800

    UnionFind: Fix bug & add demo - C++

--- UnionFind/main.cpp
+++ UnionFind/main.cpp
@@ -23,14 +23,22 @@ int GetFather(int a)
 
 void Union(int a, int b)
 {
-    if (father[a] != father[b])
-        father[father[a]] = father[b];
+    if (GetFather[a] != GetFather[b])
+        father[GetFather(a)] = GetFather(b);
 }
 
 int main()
 {
     for (int i = 0; i < MAX_N; i++)
         father[i] = i;
+
+    Union(1, 2);
+    Union(3, 4);
+    Union(5, 6);
+    // Union(1, 3);
+    Union(3, 5);
+    for (int i = 1; i <= 6; i++)
+        printf("%d's father: %d\n", i, GetFather(i));
 
     return 0;
 }
