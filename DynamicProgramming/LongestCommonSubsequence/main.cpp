fd6c3981fcf52b6134c424ca16c0dfef4452ed89
Heliovic <1005031096@qq.com>
2019-08-31 10:00:47 +0800

    DynamicProgramming: Classical dp problems - C++

--- DynamicProgramming/LongestCommonSubsequence/main.cpp
+++ DynamicProgramming/LongestCommonSubsequence/main.cpp
@@ -6,34 +6,34 @@
 
 using namespace std;
 
-char str1[MAX_N];
-char str2[MAX_N];
-
+char s1[MAX_N], s2[MAX_N];
 int dp[MAX_N][MAX_N];
 
 int main()
 {
-    gets(str1);
-    gets(str2);
+    fill_n(&dp[0][0], MAX_N * MAX_N, 0);
 
-    int len1 = strlen(str1);
-    int len2 = strlen(str2);
+    s1[0] = s2[0] = ' ';
+    scanf("%s %s", s1 + 1, s2 + 1);
 
-    int ans = dp[0][0] = str1[0] == str2[0] ? 1 : 0;
+    int len1 = strlen(s1);
+    int len2 = strlen(s2);
 
-    for (int i = 0; i < len1; i++)
+    len1--;
+    len2--;
+
+    for (int i = 1; i <= len1; i++)
     {
-        for (int j = 0; j < len2; j++)
+        for (int j = 1; j <= len2; j++)
         {
-            if (i == 0 && j == 0)
-                continue;
-            if (str1[i] == str2[j])
+            if (s1[i] == s2[j])
                 dp[i][j] = dp[i - 1][j - 1] + 1;
             else
                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-            ans = max(ans, dp[i][j]);
         }
     }
-    printf("%d", ans);
+
+    printf("%d", dp[len1][len2]);
+
     return 0;
 }
